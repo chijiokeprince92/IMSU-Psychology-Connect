a642986a804e7920bc872ad5a3b526b961c48108
Chijioke Nwaiwu <chijioke.nwaiwu92@gmail.com>
2018-10-02 16:02:16 +0100

    css

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const helmet = require('helmet');
 const mongoose = require('mongoose');
 const MongoStore = require('connect-mongo')(session);
 
-mongoose.connect(process.env.mongoDBO);
+mongoose.connect(process.env.mongoDB);
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Pearly, MongoDB connection error'));
